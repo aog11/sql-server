b47ffe3fdde340925758e53bfdc76ffc6958643e
Antoni OG <antonifco@gmail.com>
2019-11-09 22:52:58 -0400

    Passwords & logins/users

--- scripts/userPrivs.sql
+++ scripts/userPrivs.sql
@@ -9,3 +9,18 @@ WITH PASSWORD = 'Prueba01*';
 -- Assigning SYSADMIN privs to the login
 ALTER SERVER ROLE sysadmin
 ADD MEMBER dbadmin;
+
+--Changing the password of a user
+ALTER USER dbadmin
+WITH PASSWORD = 'Prueba02*';
+
+--Changing the password of a login
+ALTER LOGIN dbadmin
+WITH PASSWORD = 'Prueba02*';
+
+--Assign a login to a user
+ALTER USER dbadmin
+WITH LOGIN dbadmin;
+--Alternate way
+EXEC sp_change_users_login 'Update_One', 'dbadmin', 'dbadmin';  
+GO
